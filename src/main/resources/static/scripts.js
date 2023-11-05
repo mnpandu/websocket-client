fa2c980e3b9b5cad9d934402fd7de54e92646ef8
mnpandu <57147498+mnpandu@users.noreply.github.com>
2023-11-04 21:29:57 -0400

    updated

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,82 +1,23 @@
-var globalNotificationCount = 0;
-var username = "";
-var userQueue = "";
-
-$(document).ready(function() {
-    console.log("Index page is ready");
-    connect();
-    getUsername();
-
-    $("#globalNotifications").click(function() {
-        resetGlobalNotificationCount();
-    });
-    
-});
-
-function getUsername() {
-        $.get("/username", function (data) {
-			username = data;
-            console.log("Username: " + data); // You can use the username in any way you want
-            // Perform other actions with the username here
-  });
-}
-
 function connect() {
-     var client =  Stomp.client('ws://localhost:61614/ws');
-     const connectCallback = (frame) => {
-        console.log('Connected: ' + frame);
-        updateGlobalNotificationDisplay();
-        client.subscribe('/topic/global-notifications', function (globalMessage) {
-			console.log('globalMessage: ' + globalMessage);
-			globalNotificationCount = globalNotificationCount + 1;
-            updateGlobalNotificationDisplay();
-            showGlobalMessage(JSON.parse(globalMessage.body).content);
-        });
-        userQueue='/queue/'+username+'-messages';
-        showusername(username);
-        console.log('userQueue: ' + userQueue);
-         client.subscribe(userQueue, function (userMessage) {
-			console.log('userMessage: ' + userMessage);
-			globalNotificationCount = globalNotificationCount + 1;
-            updateGlobalNotificationDisplay();
-            showUserMessage(JSON.parse(userMessage.body).content);
-        });
-
-    };
-    
-    const errorCallback = (error) => {
-        console.error('Error: ', error);
-        setTimeout(() => {
-            console.log('Attempting to reconnect...');
-            connect();
-        }, 5000); // 5 seconds
-    };
-    
-    client.connect('admin', 'admin', connectCallback, errorCallback);
+	ws = new WebSocket('ws://localhost:8080/name');
+	ws.onmessage = function(data){
+		showGreeting(data.data);
+	}
+	 setConnected(true);
 }
 
-function showGlobalMessage(globalMessage) {
-    $("#globalMessages").append("<tr><td>" + globalMessage + "</td></tr>");
-}
-
-function showUserMessage(userMessage) {
-    $("#globalMessages").append("<tr><td>" + userMessage + "</td></tr>");
-}
-
-function showusername(username) {
-    $("#usernameValue").append("<tr><td>" + username + "</td></tr>");
-}
-
-function updateGlobalNotificationDisplay() {
-    if (globalNotificationCount == 0) {
-        $('#globalNotifications').text(globalNotificationCount);
-    } else {
-        $('#globalNotifications').show();
-        $('#globalNotifications').text(globalNotificationCount);
+function disconnect() {
+    if (ws != null) {
+        ws.close();
     }
+    setConnected(false);
+    console.log("Disconnected");
 }
 
-function resetGlobalNotificationCount() {
-    globalNotificationCount = 0;
-    updateGlobalNotificationDisplay();
+function sendName() {
+    ws.send($("#name").val());
+}
+
+function showGreeting(message) {
+    $("#greetings").append(" " + message + "");
 }
